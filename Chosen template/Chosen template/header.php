7412c0bc3c07440178338b31cf91edab8223f508
ivanspasov99 <ivan_spasov99@abv.bg>
2017-04-21 19:49:07 +0300

    new
    new

---
+++ Chosen template/Chosen template/header.php
@@ -0,0 +1,97 @@
+
+<!--
+author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Fantasy World A Games Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Fantasy World Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free web designs for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- gallery -->
+<link rel="stylesheet" href="css/lightGallery.css" type="text/css" media="all" />
+<!-- //gallery -->
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+
+<!-- //font-awesome icons -->
+<link href="//fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Jura:300,400,500,600" rel="stylesheet">
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<!-- style for footer -->
+<link href="css/style_footer.css" rel="stylesheet"> 
+<!-- end style for footer -->
+<script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
+		   
+<!-- <script
+  src="https://code.jquery.com/jquery-3.2.1.js"
+  integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+  crossorigin="anonymous"></script> -->
+
+</head>
+	
+<body>
+ <script type="text/javascript">
+			    $(document).ready(function () {
+			        $('#horizontalTab').easyResponsiveTabs({
+			            type: 'default', //Types: default, vertical, accordion           
+			            width: 'auto', //auto or any width like 600px
+			            fit: true   // 100% fit in a container
+			        });
+			    });
+				
+</script>
+<!-- header -->
+	<div class="header">
+		<div class="w3layouts_header_left">
+				<div class="top-nav-text">
+						<p>Call Us : <span class="call">+00 111 2222 333</span></p>
+						<p>Email Us : <span class="mail"><a href="mailto:info@example.com">info@example.com</a></span></p>
+					</div>
+		</div>
+	
+		<div class="clearfix"> </div>
+	</div>
+	<div class="w3_navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<div class="w3_navigation_pos">
+						<h1><a href="index.html"><span>Fantasy</span> World</a></h1>
+					</div>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+					<nav class="cl-effect-5" id="cl-effect-5">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="index.html"><span data-hover="Home">Home</span></a></li>
+
+							<li><a href="#about" class="scroll"><span data-hover="About">About</span></a></li>
+							<li><a href="#services" class="scroll"><span data-hover="Services">Services</span></a></li>
+							<li><a href="#work" class="scroll"><span data-hover="Gallery">Gallery</span></a></li>
+							<li><a href="#projects" class="scroll"><span data-hover="News">News</span></a></li>
+							<li><a href="#mail" class="scroll"><span data-hover="Contact">Contact</span></a></li>
+						</ul>
+					</nav>
+				</div>
+			</nav>	
+		</div>
+	</div>
+<!-- //header -->
