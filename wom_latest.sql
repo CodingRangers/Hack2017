be4a07de04958e6266addee75755265c8ce37f7d
Svetoslav <cvexxxxa@abv.bg>
2017-04-21 23:56:11 +0300

    latest
    latest latest

--- wom_latest.sql
+++ wom_latest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 21 апр 2017 в 22:21
+-- Generation Time: 21 апр 2017 в 22:55
 -- Версия на сървъра: 10.1.21-MariaDB
 -- PHP Version: 7.0.15
 
@@ -19,6 +19,49 @@ SET time_zone = "+00:00";
 --
 -- Database: `wom`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `characters`
+--
+
+CREATE TABLE `characters` (
+  `char_id` int(11) NOT NULL,
+  `char_name` varchar(50) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `char_xp` int(100) NOT NULL,
+  `char_lv` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Схема на данните от таблица `characters`
+--
+
+INSERT INTO `characters` (`char_id`, `char_name`, `class_id`, `char_xp`, `char_lv`) VALUES
+(1, 'WarriorCvexa', 1, 100, 1),
+(2, 'MageCvexa', 1, 100, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `class`
+--
+
+CREATE TABLE `class` (
+  `class_id` int(11) NOT NULL,
+  `class_name` varchar(50) NOT NULL,
+  `race_id` int(11) NOT NULL,
+  `class_avatar` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Схема на данните от таблица `class`
+--
+
+INSERT INTO `class` (`class_id`, `class_name`, `race_id`, `class_avatar`) VALUES
+(1, 'knight', 1, ''),
+(2, 'programmer', 1, '');
 
 -- --------------------------------------------------------
 
@@ -47,9 +90,85 @@ INSERT INTO `magics` (`magic_id`, `magic_name`, `magic_type`, `magic_avatar`, `m
 (3, 'Happiness', 2, '', 2000, 'Very strong spell against demons', 3, 1),
 (4, 'blabla', 2, '', 2000, 'Very strong spell against demons', 3, 1);
 
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `magic_type`
+--
+
+CREATE TABLE `magic_type` (
+  `type_id` int(11) NOT NULL,
+  `type` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Схема на данните от таблица `magic_type`
+--
+
+INSERT INTO `magic_type` (`type_id`, `type`) VALUES
+(1, 'attack'),
+(2, 'defence');
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `race`
+--
+
+CREATE TABLE `race` (
+  `race_id` int(11) NOT NULL,
+  `race_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Схема на данните от таблица `race`
+--
+
+INSERT INTO `race` (`race_id`, `race_name`) VALUES
+(1, 'human'),
+(2, 'Dentist');
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `user_pass` varchar(50) NOT NULL,
+  `user_avatar` varchar(200) NOT NULL,
+  `characters_id` int(11) NOT NULL,
+  `date_deleted` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Схема на данните от таблица `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_name`, `user_pass`, `user_avatar`, `characters_id`, `date_deleted`) VALUES
+(2, 'cvexa', 'cvexa', '', 2, NULL),
+(3, 'cvexa2', 'cvexa', '', 1, NULL);
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `characters`
+--
+ALTER TABLE `characters`
+  ADD PRIMARY KEY (`char_id`),
+  ADD KEY `char_race` (`class_id`),
+  ADD KEY `char_race_2` (`class_id`);
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`class_id`),
+  ADD KEY `race_id` (`race_id`);
 
 --
 -- Indexes for table `magics`
@@ -60,14 +179,80 @@ ALTER TABLE `magics`
   ADD KEY `magic_race` (`class_id`);
 
 --
+-- Indexes for table `magic_type`
+--
+ALTER TABLE `magic_type`
+  ADD PRIMARY KEY (`type_id`);
+
+--
+-- Indexes for table `race`
+--
+ALTER TABLE `race`
+  ADD PRIMARY KEY (`race_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_character` (`characters_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `characters`
+--
+ALTER TABLE `characters`
+  MODIFY `char_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `class_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `magics`
 --
 ALTER TABLE `magics`
   MODIFY `magic_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `magic_type`
+--
+ALTER TABLE `magic_type`
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `race`
+--
+ALTER TABLE `race`
+  MODIFY `race_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Ограничения за дъмпнати таблици
+--
+
+--
+-- Ограничения за таблица `characters`
+--
+ALTER TABLE `characters`
+  ADD CONSTRAINT `class` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`);
+
+--
+-- Ограничения за таблица `class`
+--
+ALTER TABLE `class`
+  ADD CONSTRAINT `race` FOREIGN KEY (`race_id`) REFERENCES `race` (`race_id`);
+
+--
+-- Ограничения за таблица `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `characters` FOREIGN KEY (`characters_id`) REFERENCES `characters` (`char_id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
