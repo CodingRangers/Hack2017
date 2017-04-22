56f9b5e32fa2d651863762f75aed522c0ad9b5f1
Svetoslav <cvexxxxa@abv.bg>
2017-04-22 04:39:13 +0300

    first character and hud load
    entering battle

--- arena.php
+++ arena.php
@@ -1,6 +1,7 @@
+
 <?php
 include_once('header.php');
-
+session_start();
 ?>
 
 <?php
@@ -8,20 +9,22 @@ $conn = mysqli_connect('localhost', 'root', '', 'wom');
 if(!$conn){
 	die("faill".mysqli_connect_error());
 }else{
-	echo "y";
+	// echo "y";
 }
 
 $i=1;
 foreach ($_GET['chosen_magics'] as $key => $value) {
 
-	echo $key." / ".$value."<br>";
+	// echo $key." / ".$value."<br>";
 	$magic[$i]=$value;
 	$i++;
 }
 $count= count($magic);
 if ($count<4) {
-
+$_SESSION['error'] = "Chose 4 magics for battle!";
 header('Location: pre-arena.php');
+}else{
+	$_SESSION['error'] = "";
 }
 
 
@@ -41,7 +44,7 @@ $read_result2 = mysqli_query($conn, $read_query2);
 		
 			// var_dump($row1);
 			
-			echo "<br>". $row2['magic_name'];
+			// echo "<br>". $row2['magic_name'];
 
 			
 
@@ -61,12 +64,43 @@ $read_result2 = mysqli_query($conn, $read_query2);
 
 
 ?>
+<style type="text/css" media="screen">
+#player_1 {
+	position:absolute;
+	background: url('img/sprites/human-knight.png') no-repeat -66px 0;
+	width: 98px;
+	height: 236px;
+	margin-bottom: 2%;
+	top:150px;
 
+}
+	
+</style>
+<div id="player_1">
 
-<?php
+</div>
+<div id="hud">
+	<img  src="img/hud.png" alt="asdasdasd" width="80%" height="auto" style="position:absolute;left:10%;bottom:0;">
+</div>
 
-include_once('footer.php')
+<script
+  src="https://code.jquery.com/jquery-3.2.1.js"
+  integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+  crossorigin="anonymous"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+    alert( "ready!" );
+    $(".header").slideUp("slow");
+   $(".w3_navigation").slideUp("slow");
+  $("#hud").fadeIn("slow");
+   $("#player_1").mouseover(function() {
+    $(this ).css({'backgroundPosition':'-264px -9px','width':'153px'})
 
+  }).mouseout(function(){
+  	$(this ).css({'backgroundPosition':'-66px 0','width':'98px'})
+  })
 
+});
 
-?>
+</script>
