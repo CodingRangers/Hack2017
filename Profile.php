844d697125ba0a723b50d710519efdc42e9eaf87
Radoslav Dimitrov <radoslav_dimitrov@outlook.com>
2017-04-22 08:21:38 +0300

    Profile page design concept

--- Profile.php
+++ Profile.php
@@ -4,10 +4,20 @@ include_once('header.php');
 ?>
 <div class="container profile">
 	<h3>Profile</h3>
-	<div class="profile_box">
+	<div class="profile_box avatar">
 		<img src="img/class-fire.jpg" alt="class">
 	</div>
-	
+	<div class="profile_box my_info">
+		<div class="my_row"><strong>name: </strong>char name</div>
+		<div class="my_row"><strong>lvl: </strong>1</div>
+		<div class="my_row"><strong>race: </strong>fire Mage</div>
+		<div class="my_row"><strong>class: </strong>mage</div>
+		<div class="my_row"><h4>Acquired skills</h4></div>
+		<div class="my_row"><img src="img/magics/fire_purgin_fire.png" alt="name"><span>purgin fire</span></div>
+		<div class="my_row"><img src="img/magics/fire_blast_shield.png" alt="name"><span>blast shield</span></div>
+		<div class="my_row"><img src="img/magics/fire_ball.png" alt="name"><span>fire ball</span></div>
+		<div class="my_row"><img src="img/magics/fire_vortex.png" alt="name"><span>fire vortex</span></div>
+	</div>
 </div>
 
 
